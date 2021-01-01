08edcdce02ee883ad4029258d0caa88897aa50a0
Mohammd-Ghazal <moh.g.ghazal@gmail.com>
2021-01-01 22:49:58 +0200

    part_1 back-end solved

--- part_1/library-app/back-end/db.js
+++ part_1/library-app/back-end/db.js
@@ -1,7 +1,6 @@
 
 const mysql = require('mysql2');
  
-// create the connection to database
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -10,7 +9,11 @@ const connection = mysql.createConnection({
  
 
 connection.connect()
-
+try {
+    const data = await sql.query('SELECT * FROM USERS ');
+} catch (error) {
+    console.log(error);
+}
 connection.query('SELECT * from books', function (err, rows, fields) {
   if (err) throw err
 
--- part_1/library-app/back-end/server.js
+++ part_1/library-app/back-end/server.js
@@ -1,10 +1,77 @@
 const express = require("express");
-
+import axios from "axios";
+import db from "db"
 const app = express();
+
+//Q1)
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'library-app'
+});
+const mysql = require('mysql2');
+
+connection.connect()
+
+
 
 const port = 3000;
 app.listen(port, () => {
-
-
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+
+
+//Q2) 
+axios.post("/books").then(function (res, req) {
+  let { title, author, pages, publisher, published_at } = req.body;
+
+connection.query(`insert into books ((title , author, pages, publisher, published_at)
+ values =${(title , author, pages, publisher, published_at)})  `,
+  function (err, rows, fields) {
+  if (err) throw err
+
+  console.log('sucess add new book')
+})
+
+});
+
+//Q3)
+axios.get("/books").then(function (res, req) {
+
+connection.query(`select * from books`, function (err, rows, fields) {
+  if (err) throw err
+
+  console.log('books: ', rows)
+})
+
+});
+
+
+//Q4)
+axios.get("/books/:book_id").then(function (res, req) {
+  let {bookId} = req.params;
+
+  connection.query(`select * from books where id = ${bookId}`, function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log('books: ', rows)
+  })
+  
+  });
+
+
+  //Q5)
+  axios.del("/books/:book_id").then(function (res, req) {
+    let {bookId} = req.params;
+  
+    connection.query(`drop column from books where id = ${bookId}`, function (err, rows, fields) {
+      if (err) throw err
+    
+      console.log('books: ', rows)
+    })
+    
+    });
+
+
+connection.end()
